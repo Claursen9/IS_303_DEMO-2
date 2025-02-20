bf15b5ecf6df2f783571a49581a14d4aeda3a4f4
Joaquin Elizalde <joaquin.n.elizalde@gmail.com>
2025-02-20 10:28:00 -0700

    Created a function to multiply 2 numbers
    takes two numbers and then multiplies them

---
+++ multiply.py
@@ -0,0 +1,8 @@
+# Joaquin Elizalde
+# 2/20/2024
+# Take in and multiply 2 numbers
+
+# Takes 2 numbers and returns the product
+def MultNums(iNum1, iNum2):
+    result = iNum1 * iNum2
+    return result
